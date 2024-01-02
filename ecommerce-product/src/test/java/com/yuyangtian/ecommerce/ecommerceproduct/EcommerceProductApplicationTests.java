a95bf12a8c9a1b87bdfd1e4b061d4f3fba20f382
tianyuyang <yuyang.tian.23@gmail.com>
2024-01-01 21:25:17 -0800

    init project

---
+++ ecommerce-product/src/test/java/com/yuyangtian/ecommerce/ecommerceproduct/EcommerceProductApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yuyangtian.ecommerce.ecommerceproduct;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceProductApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
